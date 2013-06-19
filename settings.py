824493e1b0f0b3d288518cb7802f7d54bd62aefd
Aaron W. Tabor <aarontabor50@gmail.com>
2013-06-19 19:18:49 -0300

    Opps, pushed my local configuration in settings.py by accident.  This is to reverse the changes

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 #/usr/bin/env python
 REDIS_DB = 0
 REDIS_PORT = 6379
-REDIS_HOST = 'localhost'
+REDIS_HOST = '192.168.2.201'
 
-RETWIS_HOST = 'localhost'
+RETWIS_HOST = '192.168.2.200'
 RETWIS_PORT = 8008
 
 SALT = 'retwis'
